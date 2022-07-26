545b037d9ffe1ca8985100d469c03a43d00548cb
Romeo Van Snick <romeovs@gmail.com>
2022-07-26 09:13:33 +0200

    Add index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { ImageGen } from "./images"
+export type { ImageData, ImageSource } from "./image"
+export type { Format } from "./format"
+export type { Options } from "./options"
+export type { EventName } from "./events"
